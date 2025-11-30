6c03d92553c368d1c4d1967ffda5c1c4eb987106
dhivyaprakashsivakumar1011 <dhivyaprakashsivakumar@gmail.com>
2025-11-30 22:30:00 +0530

    initial

---
+++ C_Problem_03_07.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    scanf("%d",&a);
+
+    b=a%10;
+    int c=a/10;
+
+    if(c==b)
+    {
+        printf("Success"); 
+        
+    }
+    else
+    {
+         printf("Failure");
+    }
+
+    return 0;
+}
---
+++ C_Problem_03_09.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    int a,b;
+    scanf("%d",&num);
+
+    a=num/10;
+    b=num%10;
+
+    if(a>b)
+    {
+        printf("Success");
+    }
+    else
+    {
+        printf("Failure");
+    }
+    return 0;
+}
