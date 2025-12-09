929f82a7b1081a5561e7138487875b72c758d3ab
Dhivyaprakash_S <dhivyaprakashsivakumar@gmail.com>
2025-12-09 19:29:09 +0530

    New

---
+++ C_Problem_05_13org.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,num,sum=0;
+	scanf("%d",&num);
+	for(i=0;num!=0;i++)
+	{
+		int digit=num%10;
+		sum=sum*10+digit;
+		num/=10;
+	}
+	printf("%d",sum);
+	return 0;
+}
